e8b1c628e8ed1b2b5e7502031310a8fa22e45449
diabeticus95 <diabeticus95@gmail.com>
2019-01-04 14:35:00 +0100

    populacja początkowa

--- genetic.py
+++ genetic.py
@@ -1,13 +1,16 @@
 from job_scheduler import *
+import copy
 
+jobSchedule = createJobSchedule(20, 20, 15)
+populacja = []
+for i in range(1,1000):
+	populacja.append(copy.deepcopy(jobSchedule))
 
-
-jobSchedule = createJobSchedule(5, 15, 10)
-for job in jobSchedule:
-	print(str(job.start_time) + " " + str(job.work_left) + " " + str(job.done))
-print("Working..." + '\n')
-	
-print(evaluateLoop([1,1,1], jobSchedule))
+#for job in jobSchedule:
+#	print(str(job.start_time) + " " + str(job.work_left) + " " + str(job.done))
+#print("Working..." + '\n')
+#	
+#print(evaluateLoop([1,1,1], jobSchedule))
 
 
 
